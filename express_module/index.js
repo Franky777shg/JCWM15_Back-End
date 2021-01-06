0aa95c6a36de8da1c3434e5c3ad50981bf94311e
Franky777shg <frengky.sihombing.777@gmail.com>
2021-01-06 16:03:47 +0700

    add login action

--- express_module/index.js
+++ express_module/index.js
@@ -11,8 +11,42 @@ const server = express()
 server.use(bodyParser.json())
 server.use(cors())
 
+// database
+let database = [
+    {
+        username: "Lisa",
+        password: "lisa123",
+        email: "lisa@gmail.com",
+    },
+    {
+        username: "Jennie",
+        password: "jennie123",
+        email: "jennie@yahoo.com",
+    },
+    {
+        username: "Rose",
+        password: "rose123",
+        email: "rose@hotmail.com",
+    }
+]
+
 server.get('/home', (req, res) => {
     res.status(200).send('<h1>Hello this is Home</h1>')
+})
+
+server.post('/login', (req, res) => {
+    // inputan dari user, akan masuk ke dalam req.body
+    console.log(req.body)
+    const { username, password } = req.body
+
+    let userIndex = database.findIndex(
+        (item) => item.username === username && item.password === password
+    )
+
+    // kita bisa kasih respon ketika ada kesalahan
+    if(userIndex === -1) return res.status(400).send('Invalid username or password')
+
+    res.status(200).send(database[userIndex])
 })
 
 server.listen(port, () => console.log(`Connected to port: ${port}`))
