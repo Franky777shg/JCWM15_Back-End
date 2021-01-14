0fbcfda9cd8d6e0895346ded13c0fa3b20411797
Franky777shg <frengky.sihombing.777@gmail.com>
2021-01-14 09:28:27 +0700

    prepare branch day 09

--- express_module/controllers/index.js
+++
@@ -1,9 +0,0 @@
-// import controller yang dibutuhkan
-const user = require('./userController')
-const product = require('./productController')
-
-// export controller yang dibutuhkan
-module.exports = {
-    user,
-    product
-}
--- express_module/routers/index.js
+++
@@ -1,7 +0,0 @@
-const userRouter = require('./userRouter')
-const productRouter = require('./productRouter')
-
-module.exports = {
-    userRouter,
-    productRouter
-}
