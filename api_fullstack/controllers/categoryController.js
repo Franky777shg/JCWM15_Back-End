ee6dd0d065af8f962082a74d3c166c19b1cf0610
Franky777shg <frengky.sihombing.777@gmail.com>
2021-01-27 13:10:14 +0700

    update category controller

--- api_fullstack/controllers/categoryController.js
+++ api_fullstack/controllers/categoryController.js
@@ -20,7 +20,7 @@ module.exports = {
         const parentId = parent_id ? parent_id : null
 
         try {
-            const addQuery = `INSERT INTO category (title, parent_id) VALUES (${db.escape(title)}, ${db.escape(parentId)})`
+            const addQuery = `INSERT INTO category (title, parent_id) VALUES ('${title}', ${db.escape(parentId)})`
 
             const result = await asyncQuery(addQuery)
 
@@ -43,5 +43,101 @@ module.exports = {
             console.log(err)
             res.status(400).send(err)
         }
-    }
+    },
+    getCateDetParent: async (req, res) => {
+        try {
+            const parentQuery = `SELECT c1.id, c1.title, c2.title AS parent FROM category c1
+                                LEFT JOIN category c2
+                                ON c1.parent_id = c2.id`
+
+            const result = await asyncQuery(parentQuery)
+
+            res.status(200).send(result)
+        }
+        catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+    },
+    getCateDetChild: async (req, res) => {
+        try {
+            const childQuery = `SELECT c1.id, c1.title, c2.title AS child FROM category c1
+                                LEFT JOIN category c2
+                                ON c1.id = c2.parent_id`
+
+            const result = await asyncQuery(childQuery)
+
+            res.status(200).send(result)
+        }
+        catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+    },
+    cateChildren: async (req, res) => {
+        try {
+            const query = `SELECT c1.id, c1.title, group_concat(c2.title SEPARATOR ', ') AS children FROM category c1
+                            LEFT JOIN category c2
+                            ON c1.id = c2.parent_id
+                            GROUP BY c1.title
+                            HAVING children IS NOT NULL
+                            ORDER BY c1.id`
+
+            const result = await asyncQuery(query)
+
+            res.status(200).send(result)
+        }
+        catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+    },
+    getTopNode: async (req, res) => {
+        try {
+            const query = `SELECT id, title AS top_node FROM category WHERE parent_id IS NULL`
+
+            const result = await asyncQuery(query)
+
+            res.status(200).send(result)
+        }
+        catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+    },
+    getLeavesNode: async (req, res) => {
+        try {
+            const query = `select c1.id, c1.title from category c1
+            left join category c2
+            ON c1.id = c2.parent_id
+            WHERE c2.title IS NULL`
+
+            const result = await asyncQuery(query)
+
+            res.status(200).send(result)
+        }
+        catch (err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+    },
+    getCateDetParentById: async (req, res) => {
+        try{
+            console.log('req query : ', req.query)
+
+            const query = `SELECT c1.id, c1.title, c2.title AS parent FROM category c1
+            LEFT JOIN category c2
+            ON c1.parent_id = c2.id
+            WHERE c1.id = ${req.query.id}`
+
+            const result = await asyncQuery(query)
+
+            res.status(200).send(result)
+        }
+        catch(err){
+            console.log(err)
+            res.status(400).send(err)
+        }
+    },
+    
 }
--- api_fullstack/routers/categoryRouter.js
+++ api_fullstack/routers/categoryRouter.js
@@ -5,5 +5,11 @@ const { categoryController } = require('../controllers')
 router.get('/getAllCate', categoryController.getAllCate)
 router.post('/addCate', categoryController.addCate)
 router.delete('/delCate/:id', categoryController.deleteCate)
+router.get('/parentCate', categoryController.getCateDetParent)
+router.get('/childCate', categoryController.getCateDetChild)
+router.get('/children', categoryController.cateChildren)
+router.get('/topNode', categoryController.getTopNode)
+router.get('/leavesNode', categoryController.getLeavesNode)
+router.get('/parentCatebyid', categoryController.getCateDetParentById)
 
 module.exports = router
